6a4729bd50dd15be1fb88e20b6bd6f941fb39d06
iofq <cjriddz@protonmail.com>
2021-09-01 01:13:05 -0500

    pushing old changes

--- bin/ipfs.sh
+++ bin/ipfs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-IPFS_HASH=$(docker run -d -v $(pwd):/src ipfs/go-ipfs add -Qr /src | tail -n 1)
+curl -O https://ipfs.io/ipns/dist.ipfs.io/go-ipfs/$IPFS_VERSION/go-ipfs_$IPFS_VERSION_linux-amd64.tar.gz
 echo $IPFS_HASH
 curl http://cloudflare-ipfs.com/ipfs/$IPFS_HASH
